40276d3df4de7aeee89ef5c4f5a3ee70f09c4ffd
Jason Allum <jallum@tonicdesign.com>
2015-11-16 09:40:37 -0500

    Initial Commit

---
+++ Breadboard/Breadboard.h
@@ -0,0 +1,19 @@
+//
+//  Breadboard.h
+//  Breadboard
+//
+//  Created by Jason Allum on 11/16/15.
+//  Copyright © 2015 Tonic Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Breadboard.
+FOUNDATION_EXPORT double BreadboardVersionNumber;
+
+//! Project version string for Breadboard.
+FOUNDATION_EXPORT const unsigned char BreadboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Breadboard/PublicHeader.h>
+
+
